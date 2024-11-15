b29ebb2951f4f8fefacf6957976bd7428529038e
ElCherry69 <thumasterkill2300@gmail.com>
2024-11-15 11:49:04 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -3,3 +3,11 @@ import streamlit as pd
 
 st.title("DE QUE SE TRATA")
 st.write("Esta aplicación está diseñada para [Esta apliacion se basa en la cultura del futbol y un poco del conocimiento que se tiene hasta la fecha sobre el.Hablandoles un poco sobre estadisticas de grandes equipos,jugadores que al logfrado alzar el balon de oro y paises que levantaron la copa mas preciada del mundo "La Copa del mundo"].")
+
+st.subheader("Características Principales:")
+st.write("- Interactividad: Los usuarios pueden interactuar con los datos y visualizaciones.")
+st.write("- Visualizaciones: Gráficos y mapas que facilitan la comprensión de la información.")
+st.write("- Accesibilidad: Fácil de usar para usuarios técnicos y no técnicos.")
+
+st.subheader("Objetivo:")
+st.write("Nuestro objetivo es [explicar el objetivo principal].")
