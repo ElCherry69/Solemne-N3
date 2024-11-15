9f6708560eda7b8374907002959b80705fb9da1f
ElCherry69 <thumasterkill2300@gmail.com>
2024-11-15 11:52:48 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit as pd
 
 st.title("DE QUE SE TRATA")
-st.write("[Esta apliacion se basa en la cultura del futbol y un poco del conocimiento que se tiene hasta la fecha sobre el.Hablandoles un poco sobre estadisticas de grandes equipos,jugadores que al logfrado alzar el balon de oro y paises que levantaron la copa mas preciada del mundo "La Copa del mundo"].")
+st.write('[Esta aplicación se basa en la cultura del fútbol y un poco del conocimiento que se tiene hasta la fecha sobre el. Hablándoles un poco sobre estadísticas de grandes equipos, jugadores que han logrado alzar el Balón de Oro y países que levantaron la copa más preciada del mundo "La Copa del Mundo".]')
 
 st.subheader("Características Principales:")
 st.write("- Interactividad: Los usuarios pueden interactuar con los datos y visualizaciones.")
