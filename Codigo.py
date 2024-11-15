c1d61d5c67d289bb1c826824c2f7d008834c9482
ElCherry69 <thumasterkill2300@gmail.com>
2024-11-15 12:14:35 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -4,5 +4,6 @@ st.title("FUSHIBOOL")
 
 with st.sidebar:
      st.expander("SOBRE QUÉ")
-     st.write(('Esta aplicación se basa en la cultura del fútbol y un poco del conocimiento que se tiene hasta la fecha sobre el. Hablándoles un poco sobre estadísticas de grandes equipos, jugadores que han logrado alzar el Balón de Oro y países que levantaron la copa más preciada del mundo "La Copa del Mundo",'))
+     st.write(('Esta aplicación se basa en la cultura del fútbol y un poco del conocimiento que se tiene hasta la fecha sobre el. Hablándoles un poco sobre estadísticas de grandes equipos, jugadores que han logrado alzar el Balón de Oro y países que levantaron la copa más preciada del mundo "La Copa del Mundo"'))
+     div = st.slider('Numero de bins:', 0, 10, 2)
      st.write('Bins=', div)
