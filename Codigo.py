c204d209c86450f827948f8dfc9e4b5a1b09aa42
ElCherry69 <thumasterkill2300@gmail.com>
2024-11-15 11:48:31 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -1,2 +1,5 @@
-import streamlit
-import pandas 
+import streamlit as st
+import streamlit as pd
+
+st.title("DE QUE SE TRATA")
+st.write("Esta aplicación está diseñada para [Esta apliacion se basa en la cultura del futbol y un poco del conocimiento que se tiene hasta la fecha sobre el.Hablandoles un poco sobre estadisticas de grandes equipos,jugadores que al logfrado alzar el balon de oro y paises que levantaron la copa mas preciada del mundo "La Copa del mundo"].")
