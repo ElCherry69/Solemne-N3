e6236d481c33a3cb45905566f5e37ad148a532ac
ElCherry69 <thumasterkill2300@gmail.com>
2024-11-15 12:07:27 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
-st.title("Mi Aplicación")
-
-# Crear un expander para la sección "SOBRE QUÉ"
-with st.expander("SOBRE QUÉ", expanded=False):
-    st.write(('Esta aplicación se basa en la cultura del fútbol y un poco del conocimiento que se tiene hasta la fecha sobre el. Hablándoles un poco sobre estadísticas de grandes equipos, jugadores que han logrado alzar el Balón de Oro y países que levantaron la copa más preciada del mundo "La Copa del Mundo",'))
+st.title("FUSHIBOOL")
+with st.sidebar:
+     st.expander("SOBRE QUÉ", expanded=False):
+     st.write(('Esta aplicación se basa en la cultura del fútbol y un poco del conocimiento que se tiene hasta la fecha sobre el. Hablándoles un poco sobre estadísticas de grandes equipos, jugadores que han logrado alzar el Balón de Oro y países que levantaron la copa más preciada del mundo "La Copa del Mundo",'))
