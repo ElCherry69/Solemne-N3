a4f190f4d38474bf45749595abcf579e0737e071
ElCherry69 <thumasterkill2300@gmail.com>
2024-11-19 15:19:12 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import pandas as pd
 
+# Título de la aplicación
 st.title("FUSHIBALL")
+
+# Cargar el archivo CSV con los datos de competiciones
+# Asegúrate de reemplazar 'competitions.csv' con la ruta correcta de tu archivo CSV
+competitions_data = pd.read_csv('competitions.csv')
 
 with st.sidebar:
     with st.expander("SOBRE QUÉ", expanded=False):
@@ -8,10 +14,11 @@ with st.sidebar:
                    'Hablándoles un poco sobre estadísticas de grandes equipos, jugadores que han logrado alzar el Balón de Oro y '
                    'países que levantaron la copa más preciada del mundo "La Copa del Mundo".'))
         
-        
         div = st.slider('Número de bins:', 0, 10, 2)
         st.write('Bins =', div)
- if 'Tipo de Competición' in competitions_data.columns:
+
+    # Añadir un selectbox para buscar por tipo de competición
+    if 'Tipo de Competición' in competitions_data.columns:
         competition_types = competitions_data['Tipo de Competición'].unique()  # Obtener tipos únicos de competición
         selected_competition = st.selectbox('Selecciona el tipo de competición:', competition_types)
 
