ec88f85d068722149c8c44bb5ca173289ecf07ac
ElCherry69 <thumasterkill2300@gmail.com>
2024-11-19 15:59:59 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-# Título de la aplicación
 st.title("FUSHIBALL")
 
-# Cargar el archivo CSV con los datos de competiciones
-# Asegúrate de reemplazar 'competitions.csv' con la ruta correcta de tu archivo CSV
 competitions_data = pd.read_csv('competitions.csv')
 
 with st.sidebar:
