f5572ec781311acc28dfacdde9a2b91f323a96be
ElCherry69 <thumasterkill2300@gmail.com>
2024-11-20 15:16:18 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -4,6 +4,10 @@ import pandas as pd
 st.title("FUSHIBALL")
 
 competitions_data = pd.read_csv('competitions.csv')
+ballon_dor_data = pd.read_csv('BallonDor-GoldenBall_Winners_v2.csv')
+world_cup_data = pd.read_csv('FIFA - World Cup Summary.csv')
+ucl_data = pd.read_csv('UCL_AllTime_Performance_Table - UCL_Alltime_Performance_Table.csv')
+ucl_finals_data = pd.read_csv('UCL_Finals_1955-2023 - UCL_Finals_1955-2023.csv')
 
 with st.sidebar:
     with st.expander("SOBRE QUÉ", expanded=False):
@@ -13,6 +17,10 @@ with st.sidebar:
         
         div = st.slider('Número de bins:', 0, 10, 2)
         st.write('Bins =', div)
-data = pd.read_csv('BallonDor-GoldenBall_Winners_v2.csv','FIFA - World Cup Summary.csv', 'UCL_AllTime_Performance_Table - UCL_Alltime_Performance_Table.csv', 'UCL_Finals_1955-2023 - UCL_Finals_1955-2023.csv')
-st.sidebar.header("Opciones de Filtro")
-search_title = st.sidebar.text_input("Jugador, Pais o Competición")
+
+    st.sidebar.header("Opciones de Filtro")
+    search_title = st.sidebar.text_input("Jugador, País o Competición")
+
+if search_title:
+    filtered_competitions = competitions_data[competitions_data['column_name'].str.contains(search_title, case=False)]
+    st.write(filtered_competitions)
