c8a70ac2efdacbcd178b034779f9642f61bdda62
ElCherry69 <thumasterkill2300@gmail.com>
2024-11-29 12:07:20 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -21,7 +21,7 @@ def set_background_image(image_url):
         unsafe_allow_html=True
     )
 
-set_background_image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.forbes.com%2Fsites%2Ftomsanderson%2F2024%2F11%2F19%2Freal-madrid-open-to-selling-105-million-star-with-premier-league-interest-reports-ok-diario%2F&psig=AOvVaw0mSowgnLDPJLmrsORfk8bP&ust=1732979018491000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNiT-8_0gYoDFQAAAAAdAAAAABAE')
+set_background_image('https://imageio.forbes.com/specials-images/imageserve/65be17cb80f5c798a03e7762/0x0.jpg?format=jpg&height=600&width=1200&fit=bounds')
 
 ballon_dor_data = pd.read_csv('BallonDor-GoldenBall_Winners_v2.csv')
 world_cup_data = pd.read_csv('FIFA - World Cup Summary.csv')
