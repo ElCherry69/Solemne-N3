00bd5da2edbf12fc12a00d6705c3e6554af3350a
ElCherry69 <thumasterkill2300@gmail.com>
2024-11-28 22:18:02 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -29,7 +29,7 @@ ucl_data = pd.read_csv('UCL_AllTime_Performance_Table - UCL_Alltime_Performance_
 ucl_finals_data = pd.read_csv('UCL_Finals_1955-2023 - UCL_Finals_1955-2023.csv')
 
 with st.sidebar:
-    with st.expander("SOBRE QUÉ", expanded=False):
+    with st.expander("¿De que se trata esta APP?", expanded=False):
         st.write(('Esta aplicación se basa en la cultura del fútbol y un poco del conocimiento que se tiene hasta la fecha sobre él. '
                    'Hablándoles un poco sobre estadísticas de grandes equipos, jugadores que han logrado alzar el Balón de Oro y '
                    'países que levantaron la copa más preciada del mundo "La Copa del Mundo".'))
